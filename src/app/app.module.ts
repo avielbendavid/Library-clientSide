ae14c6e00357e802463321d108608aa03542d37e
Aviel <avielbendavid22@gmail.com>
2021-03-10 09:34:32 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,19 @@ import {MatButtonModule} from '@angular/material/button';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AdminZoneComponent } from './components/admin-zone/admin-zone.component';
+import { CustomerZoneComponent } from './components/customer-zone/customer-zone.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    AdminZoneComponent,
+    CustomerZoneComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
